4ad765bd4bd9187e0af5f8c361da206d715b609a
Antonio Sabino <aslimajr@outlook.com>
2022-03-14 11:11:18 -0300

    Criaçao da funcao Login

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,7 +4,7 @@
   <div class="login">
     <input type="email" v-model="email" placeholder="Digite seu e-mail" />
     <input type="password" v-model="password" placeholder="Digite sua senha" />
-    <button>Entrar</button>
+    <button v-on:click="login">Entrar</button>
     <p>
       <router-link to="/sign-up">Criar Conta</router-link>
     </p>
@@ -12,7 +12,25 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "LoginPage",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      const result = await axios.get(
+        `http://localhost:3000/user?email=${this.email}&password=${this.password}`
+      );
+      if (result.status === 200 && result.data.length) {
+        localStorage.setItem("user-info", JSON.stringify(result.data[0]));
+        this.$router.push({ name: "HomePage" });
+      }
+    },
+  },
 };
 </script>

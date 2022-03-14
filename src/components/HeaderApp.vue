258d322bc0840da78021cc16103b734b5dde37e9
Antonio Sabino <aslimajr@outlook.com>
2022-03-14 12:49:47 -0300

    Criaçao do Header

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="nav">
+    <a href="#">Home</a>
+    <a href="#">Adicionar Restaurante</a>
+    <a href="#">Editar Restaurante</a>
+    <a href="#">Sair</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderApp",
+};
+</script>
+
+<style>
+.nav {
+  background: #333;
+  overflow: hidden;
+}
+.nav a {
+  float: left;
+  color: #f2f2f2;
+  padding: 14px 16px;
+  text-align: center;
+  font-size: 17px;
+  font-weight: bold;
+  text-decoration: none;
+  margin-right: 4px;
+  border-top: 3px solid #333;
+  border-bottom: 3px solid #333;
+  border-radius: 4px;
+}
+.nav a:hover {
+  background: #ddd;
+  color: #333;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,15 @@
 <template>
+  <HeaderApp />
   <h1>Pagina de Home</h1>
 </template>
 
 <script>
+import HeaderApp from "./HeaderApp.vue";
 export default {
   name: "HomePage",
+  components: {
+    HeaderApp,
+  },
   mounted() {
     const user = localStorage.getItem("user-info");
     if (!user) {

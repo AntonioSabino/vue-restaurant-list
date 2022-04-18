603545c0484c47826c42ec8357e6df17da2bac75
Antonio Sabino <aslimajr@outlook.com>
2022-04-18 19:05:56 -0300

    Incluindo restaurantes ao componente Home

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,26 +1,52 @@
 <template>
   <HeaderApp />
-  <h1>Olá {{name}}, você está na página de Home</h1>
+  <h1>Olá {{ name }}, você está na página de Home</h1>
+  <table border="1">
+    <tr>
+      <td>ID</td>
+      <td>Nome</td>
+      <td>Contato</td>
+      <td>Endereço</td>
+    </tr>
+    <tr v-for="restaurant in restaurants" :key="restaurant.id">
+      <td>{{restaurant.id}}</td>
+      <td>{{restaurant.name}}</td>
+      <td>{{restaurant.contact}}</td>
+      <td>{{restaurant.address}}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
 import HeaderApp from "./HeaderApp.vue";
+import axios from "axios";
+
 export default {
   name: "HomePage",
   data() {
     return {
       name: "",
+      restaurants: [],
     };
   },
   components: {
     HeaderApp,
   },
-  mounted() {
+  async mounted() {
     const user = localStorage.getItem("user-info");
     this.name = JSON.parse(user).name;
     if (!user) {
       this.$router.push({ name: "SignUp" });
     }
+    const result = await axios.get("http://localhost:3000/restaurants");
+    this.restaurants = result.data;
   },
 };
 </script>
+
+<style>
+td {
+  width: 160px;
+  height: 40px;
+}
+</style>
